860da6bd692fae19a4a4777f0173b6c2246b3ecf
Tigran0199 <cruelman11@gmail.com>
2022-05-10 14:31:41 +0400

    feat: Added routes for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
+import {BrowserRouter , Route , Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}/> {/*  Main page  */}
+          <Route path="/cinemas" element={<Cinemas />}/>{/* For cinemas  */}
+          <Route path="/concerts" element={<Concerts />}/>{/*  For concerts  */}
+          <Route path="/theaters" element={<Theaters />}/> {/*  For theaters  */}
+          <Route path="/clubsAndPubs" element={<ClubsAndPubs />}/>{/*  For clubs and pubs events  */}
+          <Route path="/register" element={<Register />}/> {/* Register page  */}
+          <Route path="/login" element={<Login />}/> {/* Login page  */}
+          <Route path="/contacts" element={<Contacts />}/>{/* About us page  */}
+          <Route path="/profile" element={<Profile />} />{/* For profile page  */}
+          <Route path="/event/" element={<Event  />} /> {/* for each event  */}
+        </Routes>
+      </BrowserRouter>
   );
 }
 
