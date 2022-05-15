e426b5aeda7bd3937bbf6ec8e5520614ac3e5f32
mariamgri22 <mariamgrigoryan99@gmail.com>
2022-05-15 14:31:00 +0400

    fix(change scss): change from styled components to scss

--- src/components/EventItems/index.jsx
+++ src/components/EventItems/index.jsx
@@ -2,33 +2,31 @@ import React from 'react'
 import event1 from '../../assets/img/event1.jpg'
 import event2 from '../../assets/img/event-2.jpg'
 import EventItem from '../EventItem'
-import styled from 'styled-components';
+import './style.scss'
 
 
-export const EventItemsStyled=styled.div`
-   display: flex;
-   flex-wrap: wrap;
-   margin-right: -15px;
-   margin-left: -15px;
-   padding:30px;
-   gap:30px;
-   justify-content: center;
-   align-items: center;
-   @media(max-width: 992px) {
-    flex-direction: column
-  }
- 
-`
 
 export default function EventItems() {
   return (
-    <EventItemsStyled>
+    <>
+    <div className='column'>
+      <div className='section'>
+        <span className='LineStyle'></span>
+        <small className='sub-title'>harmoni events</small>
+        <h2 className='big-title'>
+          <strong className='EvStrong'>event</strong>
+            Listing
+        </h2>
+      </div>
+    </div>
+    <div className='EventItemsStyled'>
        <EventItem  poster={event1} title="Barcelona Food Truck Festival 2018" description="Tickets from $52" date="Start 20:00pm - 22:00pm" location=" Manhattan, New York"/>
        <EventItem  poster={event2} title="Barcelona Food Truck Festival 2019" description="Tickets from $52" date="Start 20:00pm - 22:00pm" location=" Manhattan, New York"/>
        <EventItem  poster={event2} title="Barcelona Food Truck Festival 2019" description="Tickets from $52" date="Start 20:00pm - 22:00pm" location=" Manhattan, New York"/>
        <EventItem  poster={event1} title="Barcelona Food Truck Festival 2018" description="Tickets from $52" date="Start 20:00pm - 22:00pm" location=" Manhattan, New York"/>
        <EventItem  poster={event2} title="Barcelona Food Truck Festival 2019" description="Tickets from $52" date="Start 20:00pm - 22:00pm" location=" Manhattan, New York"/>
        <EventItem  poster={event1} title="Barcelona Food Truck Festival 2018" description="Tickets from $52" date="Start 20:00pm - 22:00pm" location=" Manhattan, New York"/>
-    </EventItemsStyled>
+    </div>
+    </>
   )
 }
