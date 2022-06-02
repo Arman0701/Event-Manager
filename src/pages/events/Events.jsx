e64d54aa88bc0b48f3b9dcd8db4af1de70d2143b
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-06-02 12:16:11 +0400

    feat: add evants page

---
+++ src/pages/events/Events.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbarelem from './../../components/navbarElements/Navbarelem';
+
+const Events = () => {
+  return (
+    <div><Navbarelem/></div>
+  )
+}
+
+export default Events
