7e304981b813857e4513a7362e37dcc4fbb16f46
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-06-02 12:16:40 +0400

    feat: add blogs page

---
+++ src/pages/blogs/Blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbarelem from './../../components/navbarElements/Navbarelem';
+
+const Blogs = () => {
+  return (
+    <div>
+      <Navbarelem/>
+    </div>
+  )
+}
+
+export default Blogs
