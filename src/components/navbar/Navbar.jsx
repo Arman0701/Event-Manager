bac927a9d278ae05f23c4054fc3fe8d6ac9fa3bb
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-06-02 12:05:14 +0400

    feat(add navbar): add navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,13 +3,14 @@ import "./navbar.scss";
 import { useState } from "react";
 import { FaSearch, FaUserAlt } from "react-icons/fa";
 import logo from "./assets/logo.png";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-
+   
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
+    isScrolled++
   };
   return (
     <div className={isScrolled ? "navbar scrolled" : "navbar"}>
@@ -19,14 +20,14 @@ const Navbar = () => {
         </div>
 
         <div className="navigation">
-          <span className="span1">HOME</span>
-          <span>ABOUT</span>
-          <span>EVENTS</span>
-          <span>BLOGS</span>
-          <span>GALLERY</span>
-          <span>SPEAKER</span>
-          <span>CONTACT</span>
-        </div>
+         <Link className="link" to="/"><span className="span1">HOME</span></Link>
+         <Link className="link" to="/about"> <span>ABOUT</span></Link>
+         <Link className="link" to="/events"> <span>EVENTS</span></Link>
+         <Link className="link" to="/blogs"> <span>BLOGS</span></Link>
+         <Link className="link" to="/gallery">  <span>GALLERY</span></Link>
+         <Link className="link" to="/speaker"><span>SPEAKER</span></Link>
+         <Link className="link" to="/contact"> <span>CONTACT</span></Link>                 
+       </div>
         <div className="right ">
           <span>
             <FaUserAlt />
