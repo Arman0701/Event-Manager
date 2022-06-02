881bcfa25d390877bf670222ebbbb8431e28be8c
Tigran0199 <cruelman11@gmail.com>
2022-06-02 12:14:29 +0400

    fix: fixed issues with wrong banches

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React from "react";
 import Home from "./pages/Home";
-import './app.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <div className="div1">
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      </div>
-      
-    </div>
-  );
+ 
 }
 
 export default App;
