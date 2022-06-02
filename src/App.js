eb7a9d1a8897f1c52642aa11cd171973dd4a0d29
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-06-02 12:22:42 +0400

    feat: modify app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from "react";
-import Home from "./pages/Home";
-import './app.scss'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./app.scss";
+import About from "./pages/about/About";
+import Blogs from "./pages/blogs/Blogs";
+import Contact from "./pages/contact/Contact";
+import Events from "./pages/events/Events";
+import Gallery from "./pages/gallery/Gallery";
+import Home from "./pages/home/Home";
+import Speaker from "./pages/speaker/Speaker";
 
 function App() {
+
+
   return (
     <div className="App">
-      <Home />
-      <div className="div1">
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      <div>Arman Argisht</div>
-      </div>
-      
+     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            {/* <Route path="/login" element={<Login/>} /> */}
+
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="events" element={<Events />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="gallery" element={<Gallery />} />
+            <Route path="speaker" element={<Speaker />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  
     </div>
   );
 }
 
 export default App;
+
+
