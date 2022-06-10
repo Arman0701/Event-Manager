480dc3e72dbadefe78612796c5c9a3884e42fb86
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-06-10 12:10:44 +0400

    feat: added event component

---
+++ src/components/event/Event.jsx
@@ -0,0 +1,39 @@
+import "./event.scss";
+import { Link } from "react-router-dom";
+import EventSearch from "./EventSearch";
+import Section from "./sectionEvent/Section";
+import useFetch from './../../hooks/useFetch';
+
+const Event = () => {
+
+ 
+
+  return (
+    <>
+      <div className="img-container">
+        <div className="titleDiv">
+          <span>HARMONI EVENTS</span>
+          <h2>
+            HARMONI <span>EVENT</span>
+          </h2>
+
+          <div className="navHome">
+            <span className="spanHome">
+              <Link style={{ textDecoration: "none" }} to="/">
+                <span className="spanHome"> Home </span>
+              </Link>
+            </span>
+            <hr />
+            <span>Event Listing</span>
+          </div>
+        </div>
+        <img src="/assets/mountain.jpg" alt="" className="img" />
+      </div>
+
+      <EventSearch />
+      <Section />
+    </>
+  );
+};
+
+export default Event;
