e519c4a4b06182a354e6a831e3d054d6e61db2da
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-06-10 12:54:09 +0400

    feat: added pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import './App.css';
-import {BrowserRouter , Route , Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/about/About";
+import Blogs from "./pages/blogs/Blogs";
+import Contact from "./pages/contact/Contact";
+import Events from "./pages/events/Events";
+import Gallery from "./pages/gallery/Gallery";
+import Home from "./pages/home/Home";
+import Speaker from "./pages/speaker/Speaker";
 
 function App() {
+
+
   return (
-    <BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />}/> {/*  Main page  */}
-          <Route path="/cinemas" element={<Cinemas />}/>{/* For cinemas  */}
-          <Route path="/concerts" element={<Concerts />}/>{/*  For concerts  */}
-          <Route path="/theaters" element={<Theaters />}/> {/*  For theaters  */}
-          <Route path="/clubsAndPubs" element={<ClubsAndPubs />}/>{/*  For clubs and pubs events  */}
-          <Route path="/register" element={<Register />}/> {/* Register page  */}
-          <Route path="/login" element={<Login />}/> {/* Login page  */}
-          <Route path="/contacts" element={<Contacts />}/>{/* About us page  */}
-          <Route path="/profile" element={<Profile />} />{/* For profile page  */}
-          <Route path="/event/" element={<Event  />} /> {/* for each event  */}
+          <Route path="/">
+            {/* <Route path="/login" element={<Login/>} /> */}
+
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="events" element={<Events />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="gallery" element={<Gallery />} />
+            <Route path="speaker" element={<Speaker />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
         </Routes>
       </BrowserRouter>
+  
+    </div>
   );
 }
 
 export default App;
+
+
---
+++ src/pages/events/Events.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Event from '../../components/event/Event';
+import Navbarelem from './../../components/navbarElements/Navbarelem';
+import "./events.scss"
+import useFetch from './../../hooks/useFetch';
+
+const Events = () => {
+
+
+
+   
+
+  return (
+    <div>
+      <Navbarelem/>
+      <div className='imgContainer' >
+
+        <Event/>
+      </div>
+   
+    
+    </div>
+  )
+}
+
+export default Events
