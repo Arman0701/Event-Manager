138d0ac1141bb04301b32fe39aed0418a01fe464
mariamgri22 <mariamgrigoryan99@gmail.com>
2022-06-11 19:43:13 +0400

    feat(add contact and pagination): add usePagination hook

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
+import { useState } from 'react';
 import EventTitle from '../EventTitle'
 import './style.scss'
 
+
+const FORM_ENDPOINT = "";
+
 export default function ContactUs() {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <div className="text-2xl">Thank you!</div>
+        <div className="text-md"> We'll be in touch soon.</div>
+      </>
+    );
+  }
   return (
     <div className='contact'>
         <div className='cntTitle'>
         <small className='cntSmall'>contact us</small>
      <EventTitle title2='Keep In Touch'  strongtitle=' With Harmoni'/>
         </div>
-       
-      <div className='contactContainer'>
+       <form
+       action={FORM_ENDPOINT}
+       onSubmit={handleSubmit}
+       method="POST"
+       target="_blank"
+       >
+       <div className='contactContainer'>
           <div>
           <div className='itemInp'>
           <input placeholder='your name'></input>
@@ -32,6 +56,8 @@ export default function ContactUs() {
       <textarea placeholder='your message'></textarea>
       <button className='contactBut'>send mail</button>
       </div>
+       </form>
+    
       
     </div>
   )
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbarelem from './../../components/navbarElements/Navbarelem';
 import Footer from "../../components/Footer/Footer"
+import ContactUs from '../../components/ContactUs';
 
 const Contact = () => {
   return (
     <div>
       <Navbarelem/>
+       <ContactUs/>
       <Footer />
     </div>
   )
