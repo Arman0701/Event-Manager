404507628441c562da5b838a48055d658e4bbe00
mariamgri22 <mariamgrigoryan99@gmail.com>
2022-06-11 20:28:40 +0400

    feat(add loader): loader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./pages/about/About";
-import Blogs from "./pages/blogs/Blogs";
-import Contact from "./pages/contact/Contact";
-import Events from "./pages/events/Events";
-import Gallery from "./pages/gallery/Gallery";
-import Home from "./pages/home/Home";
-import Speaker from "./pages/speaker/Speaker";
+import { lazy, Suspense } from "react";
+const About=lazy(()=>import("./pages/about/About"));
+const Blogs=lazy(()=>import ("./pages/blogs/Blogs"));
+const Contact=lazy(()=>import ("./pages/contact/Contact"));
+const Events=lazy(()=> import ("./pages/events/Events"));
+const Gallery=lazy(()=>import (  "./pages/gallery/Gallery"));
+const Home=lazy(()=>import("./pages/home/Home"));
+const Speaker=lazy(()=>import("./pages/speaker/Speaker"));
 import "./App.css";
 import React from "react";
+import Loader from './assets/img/preloader.gif'
+
 
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
+      <Suspense fallback ={<div className="loaderGif"><img src={Loader}></img></div>}>
         <Routes>
           <Route path="/">
             {/* <Route path="/login" element={<Login/>} /> */}
@@ -26,6 +30,7 @@ function App() {
             <Route path="contact" element={<Contact />} />
           </Route>
         </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
