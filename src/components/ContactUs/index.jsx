8d1d95fc6b31a1c825cf6e5412c91e4840cea29a
Tigran0199 <cruelman11@gmail.com>
2022-06-12 19:15:37 +0400

    fix: fixed eslint issue

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -18,7 +18,7 @@ export default function ContactUs() {
     return (
       <>
         <div className="text-2xl">Thank you!</div>
-        <div className="text-md"> We'll be in touch soon.</div>
+        <div className="text-md"> We&apos;ll be in touch soon.</div>
       </>
     );
   }
