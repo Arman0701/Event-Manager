3fcec55cc49040036d3d263fd50117106c1b3e68
Arman0701 <gittadevosyan889@gmail.com>
2023-01-10 21:57:13 +0400

    feat(): Project was deployed to Firebase Hosting service.

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7fng283oaYHckndpI5ioOCIJUVyWoORs",
+    authDomain: "events-manage-app.firebaseapp.com",
+    projectId: "events-manage-app",
+    storageBucket: "events-manage-app.appspot.com",
+    messagingSenderId: "431400152743",
+    appId: "1:431400152743:web:4180a43dae91bb6f907582",
+};
+
+const app = initializeApp(firebaseConfig);
